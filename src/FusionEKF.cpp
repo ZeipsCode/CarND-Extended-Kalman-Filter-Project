f61c19c4b98e52906dafd147a1aa32817e39032c
zpr <Dennis.Zeipert@streetscooter.eu>
2018-06-25 14:45:55 +0200

    small update

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,6 +36,8 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+
+
     ekf_.P_ = MatrixXd(4, 4);
     ekf_.P_ << 1, 0, 0, 0,
                0, 1, 0, 0,
@@ -44,6 +46,8 @@ FusionEKF::FusionEKF() {
     
     H_laser_ << 1, 0, 0, 0,
                 0, 1, 0, 0;
+
+
 
 }
 
@@ -71,9 +75,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+
+
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+
+    	
         double rho = measurement_pack.raw_measurements_[0];
         double phi = measurement_pack.raw_measurements_[1];
         double rhoD = measurement_pack.raw_measurements_[2];
@@ -90,16 +98,20 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
         ekf_.x_ << px, py, vx, vy;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+
       /**
       Initialize state.
       */
+
         ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
     }
 
     // done initializing, no need to predict or update
+
     previous_timestamp_ = measurement_pack.timestamp_;
     is_initialized_ = true;
     return;
+
   }
 
   /*****************************************************************************
